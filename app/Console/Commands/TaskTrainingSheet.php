e7a4f8081bbbfedf1bb68d87b8f14ac3e68ac2e8
Jorgito Paiva <jspaiva.1977@gmail.com>
2022-01-08 22:16:36 -0300

    Criação do Schedul TaskTrainingSheet

---
+++ app/Console/Commands/TaskTrainingSheet.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\TrainingSheets;
+use Illuminate\Console\Command;
+
+class TaskTrainingSheet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'task:trainingsheet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando para verificar e atualizar a data de validade de Ficha de Treinamento';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $trainingSheet = new TrainingSheets();
+        $ts = $trainingSheet->all();
+
+        $diaAtual = date("Y-m-d");
+
+        
+        foreach ($ts as $value) {
+
+            if (strtotime($diaAtual) > strtotime($value['end_date'])) {
+
+                $t = $trainingSheet->findOrFail($value['id']);
+                $t->update(['active' => 'Não']);
+                
+            }
+            
+        }
+    }
+}
