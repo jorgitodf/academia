f43171faa7c918140ec2eb5faa1ae5f797eae8b1
Jorgito Paiva <jspaiva.1977@gmail.com>
2022-01-18 10:40:07 -0300

    CRUD Group Exercise Finalizado

--- app/Validations/ValidationGroupExercise.php
+++ app/Validations/ValidationGroupExercise.php
@@ -2,39 +2,39 @@
 
 namespace App\Validations;
 
-use App\Helpers\Helpers;
-
 class ValidationGroupExercise
 {
     private $erros = [];
 
     public function validateGroupExercise($data, $model = null, $action = null, $id = null)
     {
-        if ($id !== null) {
-            $groupExercise = $model->find($id);
-            if ($groupExercise === null || !is_numeric($id)) {
-                $this->erros['error-group-exercise'] = "Grupo de Exercício não Encontrado!";
-            }
-        }
-
         try {
             $groupExercise = $model->where('name', $data['name'])->first();
         } catch (\Exception $e) {
             return ['error' => $e->getMessage(), 'code' => 500];
         }
 
-        if (!isset($data['name']) || empty($data['name'])) {
-            $this->erros['error-name'] = "Informe o Grupo de Exercício!";
-        } else if ($groupExercise !== null && $action !== 'PUT') {
-            $this->erros['error-name'] = "O Grupo de Exercício " . $groupExercise->name . " já existe!";
-        } else {
+        if ($id !== null) {
 
-            if (preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $data['name'])) {
-                $this->erros['error-name'] = "O Campo Nome do Grupo de Exercício não pode conter número(s)!";
+            $groupExercise = $model->find($id);
+            if ($groupExercise === null || !is_numeric($id)) {
+                $this->erros['error-group-exercise'] = "Grupo de Exercício não Encontrado!";
             }
 
-        }
+        } else {
 
+            if (!isset($data['name']) || empty($data['name'])) {
+                $this->erros['error-name'] = "Informe o Grupo de Exercício!";
+            } else if ($groupExercise !== null && $action !== 'PUT') {
+                $this->erros['error-name'] = "O Grupo de Exercício " . $groupExercise->name . " já existe!";
+            } else {
+
+                if (preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $data['name'])) {
+                    $this->erros['error-name'] = "O Campo Nome do Grupo de Exercício não pode conter número(s)!";
+                }
+
+            }
+        }
         return $this->erros;
     }
 
